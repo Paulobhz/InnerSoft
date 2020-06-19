9527cb5a667242b1fbd6a698ed8ab9d337a3a06f
Paulobhz <paulobhz7@gmail.com>
2020-06-19 12:20:21 -0300

    Inicial

---
+++ uMain.pas
@@ -0,0 +1,25 @@
+unit uMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects;
+
+type
+  TfrmMain = class(TForm)
+    Image1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.fmx}
+
+end.
